6fb86c6640c9fc26e6fcd5cc64479ae0cadff47b
osamadweikat <osamadweikat83@gmail.com>
2025-07-23 16:39:05 +0300

    add error handling middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const books = require("./routes/books");
 const authors = require("./routes/authors");
 const mongoose = require("mongoose");
 const logger = require("./middlewares/logger");
+const {notFound, errorHandler} = require("./middlewares/errors");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -17,6 +18,9 @@ app.use(express.json());
 app.use(logger);
 app.use("/api/books", books);
 app.use("/api/authors", authors);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
