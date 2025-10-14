b787054bff60f4f5eb3c4f81df0a050aa5206858
osamadweikat <osamadweikat83@gmail.com>
2025-10-14 15:22:01 +0300

    add top header styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import "./header.css"
+import "./header.css";
 
 export default function Header() {
   return (
@@ -7,7 +7,9 @@ export default function Header() {
         <div className="header-top-menu">
           <i className="bi bi-list"></i>
         </div>
-        <div className="header-top-phone">0123-456-789</div>
+        <div className="header-top-phone">
+          <i className="bi bi-telephone-fill"></i>0123-456-789
+        </div>
         <div className="header-top-text">Welcome To Online Book Store</div>
         <div className="header-top-link">
           <i className="bi bi-person-fill"></i>Login
