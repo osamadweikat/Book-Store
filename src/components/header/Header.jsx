b9f6b4f1a83ffe09a7f4a17b0ff84ccb87803152
osamadweikat <osamadweikat83@gmail.com>
2025-10-14 17:24:42 +0300

    make header responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,16 @@
+import { useState } from "react";
 import "./header.css";
 import HeaderMiddle from "./HeaderMiddle";
 import HeaderTop from "./HeaderTop";
 import Navbar from "./Navbar";
 
 export default function Header() {
+  const [toggle, setToggle] = useState(false);
   return (
     <div className="header">
-      <HeaderTop />
+      <HeaderTop toggle={toggle} setToggle={setToggle} />
       <HeaderMiddle />
-      <Navbar />
+      <Navbar toggle={toggle} setToggle={setToggle} />
     </div>
   );
 }
