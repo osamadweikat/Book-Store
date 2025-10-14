fab2acc9d811ea88790bc4238c99f2ecfc65625f
osamadweikat <osamadweikat83@gmail.com>
2025-10-14 14:43:07 +0300

    create header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import "./header.css"
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="header-top">
+        <div className="header-top-menu">
+          <i className="bi bi-list"></i>
+        </div>
+        <div className="header-top-phone">0123-456-789</div>
+        <div className="header-top-text">Welcome To Online Book Store</div>
+        <div className="header-top-link">
+          <i className="bi bi-person-fill"></i>Login
+        </div>
+      </div>
+    </div>
+  );
+}
