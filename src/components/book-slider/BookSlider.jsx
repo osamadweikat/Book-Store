e43e47431e1eaa1bdc4d8650fa867f71aac3deac
osamadweikat <osamadweikat83@gmail.com>
2025-10-15 12:54:27 +0300

    add functionality to  book slider

--- src/components/book-slider/BookSlider.jsx
+++ src/components/book-slider/BookSlider.jsx
@@ -1,27 +1,45 @@
 import "./book-slider.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
 
 export default function BookSlider({ data }) {
   return (
     <div className="book-slider-container">
-      <i className="bi bi-chevron-left book-slider-arrow-left"></i>
-      <div className="book-slider-wrapper">
+      <Swiper
+        modules={[Navigation]}
+        navigation={{
+          nextEl: ".book-slider-arrow-right",
+          prevEl: ".book-slider-arrow-left",
+        }}
+        loop={true}
+        slidesPerView={4}
+        spaceBetween={20}
+        speed={600}
+        className="book-slider-wrapper"
+      >
         {data.map((item) => (
-          <div key={item.id} className="book-slide-item">
-            <img
-              src={`/books/${item.image}`}
-              alt={item.title}
-              className="book-slide-item-img"
-            />
-            <h3 className="book-slide-item-title">{item.title}</h3>
-            <div className="rating">rating</div>
-            <div className="book-slide-item-price">${item.price}</div>
-            <div className="book-slider-icons-wrapper">
-              <i className="bi bi-eye-fill"></i>
-              <i className="bi bi-cart-plus"></i>
+          <SwiperSlide key={item.id}>
+            <div className="book-slide-item">
+              <img
+                src={`/books/${item.image}`}
+                alt={item.title}
+                className="book-slide-item-img"
+              />
+              <h3 className="book-slide-item-title">{item.title}</h3>
+              <div className="rating">rating</div>
+              <div className="book-slider-item-price">${item.price}</div>
+              <div className="book-slider-icons-wrapper">
+                <i className="bi bi-eye-fill"></i>
+                <i className="bi bi-cart-plus"></i>
+              </div>
             </div>
-          </div>
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
+
+      <i className="bi bi-chevron-left book-slider-arrow-left"></i>
       <i className="bi bi-chevron-right book-slider-arrow-right"></i>
     </div>
   );
