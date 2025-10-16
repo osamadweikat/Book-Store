74921ee0ab780b053b34aade121ebe5403a33d5c
osamadweikat <osamadweikat83@gmail.com>
2025-10-16 23:14:13 +0300

    refactor: simplify Cart component structure

---
+++ src/pages/cart/OrderSummary.jsx
@@ -0,0 +1,23 @@
+export default function OrderSummary({ totalPrice }) {
+  return (
+    <div className="cart-order-summary">
+      <div className="order-summary-title">ORDER SUMMARY</div>
+      <div className="order-summary-item">
+        <span>Subtotal</span>
+        <span>${totalPrice}</span>
+      </div>
+      <div className="order-summary-item">
+        <span>Shopping Cost</span>
+        <span>0</span>
+      </div>
+      <div className="order-summary-item">
+        <span>Discount</span>
+        <span>0</span>
+      </div>
+      <div className="order-summary-item">
+        <span>Total</span>
+        <span>${totalPrice}</span>
+      </div>
+    </div>
+  );
+}
