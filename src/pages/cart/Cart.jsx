044210acaf99d32fd618f8d8e7281d832f6d2330
osamadweikat <osamadweikat83@gmail.com>
2025-10-16 21:56:02 +0300

    create cart component

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,5 +1,42 @@
+import { cartInfo } from "../../data/cart";
+
 export default function Cart() {
   return (
-    <h1>Cart Page</h1>
-  )
+    <div className="cart">
+      <div className="cart-title">Your Shopping Cart</div>
+      <div className="cart-wrapper">
+        <div className="cart-items">
+          {cartInfo.map((item) => (
+            <div key={item.id} className="cart-items">
+              <img
+                src={`/books/${item.image}`}
+                alt={item.title}
+                className="cart-item-img"
+              />
+              <div className="cart-item-info">
+                <div>
+                  <div className="cart-item-book-title">
+                    <b>Title: </b> {item.title}
+                  </div>
+                  <div className="cart-item-author">
+                    <b>Author: </b> {item.author}
+                  </div>
+                </div>
+                <div className="cart-item-quantity">
+                  <button>
+                    <i className="bi bi-plus-lg"></i>
+                  </button>
+                  <b>{item.quantity}</b>
+                  <button>
+                    <i className="bi bi-dash-lg"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="cart-order-summary">Order Summary</div>
+    </div>
+  );
 }
