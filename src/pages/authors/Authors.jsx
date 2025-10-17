458b97b133e2d596f07a1296cae84e1d32ac5a6b
osamadweikat <osamadweikat83@gmail.com>
2025-10-17 12:58:46 +0300

    create authors component

--- src/pages/authors/Authors.jsx
+++ src/pages/authors/Authors.jsx
@@ -1,5 +1,20 @@
+import { authors } from "../../data/authors";
+import "./authors.css";
+
 export default function Authors() {
   return (
-    <h1>Authors Page</h1>
-  )
+    <section className="authors">
+      <div className="authors-search-wrapper">
+        <input type="search" placeholder="Search in authors" />
+      </div>
+      <div className="authors-wrapper">
+        {authors.map((author) => (
+          <div key={author.id} className="author">
+            <img src={author.image} alt={author.name} className="author-img" />
+            <h2 className="author-name">{author.name}</h2>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
