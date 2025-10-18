d2ab3c9893843f83fcc85986b7096a878eeeeeac
osamadweikat <osamadweikat83@gmail.com>
2025-10-18 23:50:09 +0300

    add contact styles

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -9,21 +9,23 @@ export default function Contact() {
             <i className="bi bi-house-fill"></i>
             Address
           </div>
-          <p className="contact-item-text">Iraq-Baghdad-Alrashid St</p>
+          <p className="contact-item-text">
+            Palestine - Nablus - Rafidia Street
+          </p>
         </div>
         <div className="contact-item">
           <div className="contact-item-icon">
             <i className="bi bi-telephone-fill"></i>
             Phone
           </div>
-          <p className="contact-item-text">123-456-789</p>
+          <p className="contact-item-text">0123-456-789</p>
         </div>
         <div className="contact-item">
           <div className="contact-item-icon">
             <i className="bi bi-envelope-fill"></i>
             Email
           </div>
-          <p className="contact-item-text">info@fakeemail.com</p>
+          <p className="contact-item-text">info@bookstore.com</p>
         </div>
       </div>
       <form onSubmit={(e) => e.preventDefault()} className="contact-form">
