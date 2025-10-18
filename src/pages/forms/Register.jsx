317464ecf17b8fa1747199c42000396939dc9c4f
osamadweikat <osamadweikat83@gmail.com>
2025-10-19 00:21:59 +0300

    create register page

--- src/pages/forms/Register.jsx
+++ src/pages/forms/Register.jsx
@@ -1,5 +1,67 @@
+import { Link } from "react-router-dom";
+import "./forms.css";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (email.trim() === "") {
+      return toast.error("Email is required");
+    }
+
+    if (username.trim() === "") {
+      return toast.error("Username is required");
+    }
+
+    if (password.trim() === "") {
+      return toast.error("Password is required");
+    }
+
+    console.log({ email, password, username });
+    setEmail("");
+    setPassword("");
+    setUsername("");
+  };
   return (
-    <h1>Register Page</h1>
-  )
+    <div className="form-wrapper">
+      <ToastContainer theme="colored" />
+      <h1 className="form-title">Create new account</h1>
+      <form onSubmit={formSubmitHandler} className="form">
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          type="text"
+          placeholder="Username"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+        />
+        <button className="form-btn" type="submit">
+          Register
+        </button>
+      </form>
+      <div className="form-footer">
+        Already have an account ?{" "}
+        <Link to="/login" className="forms-link">
+          Login
+        </Link>
+      </div>
+    </div>
+  );
 }
